79866e9f055a3699d4a73531cdb33b0e0395c2c9
Beyza Cebeci <beyzacebeci34@gmail.com>
2023-05-06 22:39:15 +0300

    code refactored

---
+++ script.js
@@ -0,0 +1,8 @@
+// typed.js effect
+let typed = new Typed('.auto-text', {
+    strings: ['I am a computer engineering student.'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 2000,
+    loop: true
+})
